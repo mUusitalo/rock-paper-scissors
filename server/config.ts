ba21edd286aa1c181f74c50bcc7336d8c77da6ec
mUusitalo <mUusitalo@users.noreply.github.com>
2023-01-15 18:29:37 +0200

    Add basic game data structure

--- server/config.ts
+++ server/config.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config({path: './.env'});
 
 const { SERVER_PORT } = process.env
+const ROUND_DURATION = 50
 
 export {
-  SERVER_PORT
+  ROUND_DURATION,
+  SERVER_PORT,
 }
