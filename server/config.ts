855444e6cf2954bd14682a0f53dc69c9b404be99
mUusitalo <mUusitalo@users.noreply.github.com>
2023-01-15 19:28:13 +0200

    Add ROUND_COUNT and DRAW_ROUND_CUTOFF constants

--- server/config.ts
+++ server/config.ts
@@ -3,8 +3,12 @@ dotenv.config({path: './.env'});
 
 const { SERVER_PORT } = process.env
 const ROUND_DURATION = 50
+const ROUND_COUNT = 999
+const DRAW_ROUND_CUTOFF = 5000
 
 export {
   ROUND_DURATION,
+  ROUND_COUNT,
+  DRAW_ROUND_CUTOFF,
   SERVER_PORT,
 }
