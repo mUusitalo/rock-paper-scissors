4cfb145f91f577742cca82918395ed936b8dcbab
mUusitalo <mUusitalo@users.noreply.github.com>
2023-01-17 01:28:29 +0200

    Implement proper example bot with Node

---
+++ bots/example-node/index.ts
@@ -0,0 +1,35 @@
+import { io } from 'socket.io-client'
+import dotenv from 'dotenv'
+dotenv.config({path: '../../.env'})
+
+const { BASE_URL, SERVER_PORT } = process.env
+const SERVER_URL = `${BASE_URL}:${SERVER_PORT}`
+
+export const Moves = {
+  ROCK: 0,
+  PAPER: 1,
+  SCISSORS: 2,
+} as const
+
+export type Move = keyof typeof Moves
+
+type RoundResult = {
+  you?: Move,
+  opponent?: Move,
+  result: 'win' | 'loss' | 'draw'
+}
+
+function main() {
+  const socket = io(SERVER_URL)
+  
+  let roundIndex = 0
+
+  socket.on('round', (previousRound: RoundResult | undefined) => {
+    const possibleMoves = Object.keys(Moves) as Move[]
+    const move = possibleMoves[roundIndex % possibleMoves.length]
+    socket.emit('move', move)
+    roundIndex++
+  })
+}
+
+main()
